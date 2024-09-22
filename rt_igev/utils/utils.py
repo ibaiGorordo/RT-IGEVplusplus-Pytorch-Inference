93925cfb32c1f47b24cb245ce1f540a01703913c
Ibai Gorordo <43162939+ibaiGorordo@users.noreply.github.com>
2024-09-22 15:42:57 +0900

    Update utils.py

--- rt_igev/utils/utils.py
+++ rt_igev/utils/utils.py
@@ -1,8 +1,5 @@
 import torch
 import torch.nn.functional as F
-import numpy as np
-from scipy import interpolate
-
 
 class InputPadder:
     """ Pads images such that dimensions are divisible by 8 """
@@ -25,36 +22,6 @@ class InputPadder:
         c = [self._pad[2], ht-self._pad[3], self._pad[0], wd-self._pad[1]]
         return x[..., c[0]:c[1], c[2]:c[3]]
 
-def forward_interpolate(flow):
-    flow = flow.detach().cpu().numpy()
-    dx, dy = flow[0], flow[1]
-
-    ht, wd = dx.shape
-    x0, y0 = np.meshgrid(np.arange(wd), np.arange(ht))
-
-    x1 = x0 + dx
-    y1 = y0 + dy
-    
-    x1 = x1.reshape(-1)
-    y1 = y1.reshape(-1)
-    dx = dx.reshape(-1)
-    dy = dy.reshape(-1)
-
-    valid = (x1 > 0) & (x1 < wd) & (y1 > 0) & (y1 < ht)
-    x1 = x1[valid]
-    y1 = y1[valid]
-    dx = dx[valid]
-    dy = dy[valid]
-
-    flow_x = interpolate.griddata(
-        (x1, y1), dx, (x0, y0), method='nearest', fill_value=0)
-
-    flow_y = interpolate.griddata(
-        (x1, y1), dy, (x0, y0), method='nearest', fill_value=0)
-
-    flow = np.stack([flow_x, flow_y], axis=0)
-    return torch.from_numpy(flow).float()
-
 
 def bilinear_sampler(img, coords, mode='bilinear', mask=False):
     """ Wrapper for grid_sample, uses pixel coordinates """
@@ -73,25 +40,3 @@ def bilinear_sampler(img, coords, mode='bilinear', mask=False):
     if mask:
         mask = (xgrid > -1) & (ygrid > -1) & (xgrid < 1) & (ygrid < 1)
         return img, mask.float()
-
-    return img
-
-
-def coords_grid(batch, ht, wd):
-    coords = torch.meshgrid(torch.arange(ht), torch.arange(wd))
-    coords = torch.stack(coords[::-1], dim=0).float()
-    return coords[None].repeat(batch, 1, 1, 1)
-
-
-def upflow8(flow, mode='bilinear'):
-    new_size = (8 * flow.shape[2], 8 * flow.shape[3])
-    return  8 * F.interpolate(flow, size=new_size, mode=mode, align_corners=True)
-
-def gauss_blur(input, N=5, std=1):
-    B, D, H, W = input.shape
-    x, y = torch.meshgrid(torch.arange(N).float() - N//2, torch.arange(N).float() - N//2)
-    unnormalized_gaussian = torch.exp(-(x.pow(2) + y.pow(2)) / (2 * std ** 2))
-    weights = unnormalized_gaussian / unnormalized_gaussian.sum().clamp(min=1e-4)
-    weights = weights.view(1,1,N,N).to(input)
-    output = F.conv2d(input.reshape(B*D,1,H,W), weights, padding=N//2)
-    return output.view(B, D, H, W)
